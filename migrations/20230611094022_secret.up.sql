7a121eeb23e3fb9bc1f2b58e5a9bf430cde4a807
David Garay <garaekz@gmail.com>
2023-06-11 03:49:36 -0700

    🚩 Delete identifier from migration

--- migrations/20230611094022_secret.up.sql
+++ migrations/20230611094022_secret.up.sql
@@ -1,7 +1,6 @@
 -- Create a postgresql secrets table for storing secrets
 CREATE TABLE secrets (
     id         VARCHAR PRIMARY KEY,
-    identifier VARCHAR NOT NULL,
     encrypted_data VARCHAR NOT NULL,
     ttl        INTEGER NOT NULL,
     created_at TIMESTAMP NOT NULL,
